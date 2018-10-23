f79bde089694ef00988e01f56f7a54bc7aa1734f
Ahmed Baly <Ahmedbaly.15@gmail.com>
2018-10-23 04:05:45 +0100

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { NavController, ActionSheetController, LoadingController } from 'ionic-angular';
+import { Camera, PictureSourceType } from '@ionic-native/camera';
+import * as Tesseract from 'tesseract.js'
+import { NgProgress } from '@ngx-progressbar/core';
+ 
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  selectedImage: string;
+  imageText: string;
+  constructor(public navCtrl: NavController, 
+    private camera: Camera,
+     private actionSheetCtrl: ActionSheetController,
+     public loadingController : LoadingController,
+      public progress: NgProgress) {
+  }
+ 
+  selectSource() {    
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Use Library',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        }, {
+          text: 'Capture Image',
+          handler: () => {
+            this.getPicture(this.camera.PictureSourceType.CAMERA);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+ 
+  getPicture(sourceType: PictureSourceType) {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
+ 
+  recognizeImage() {
+
+    let loader = this.loadingController.create({
+      content: 'Getting data...',
+      dismissOnPageChange: true
+  });
+  loader.present();
+
+    Tesseract.recognize(this.selectedImage)
+    .progress(message => {
+      if (message.status === 'recognizing text')
+      this.progress.set(message.progress);
+    })
+    .catch(err => console.error(err))
+    .then(result => {
+      this.imageText = result.text;
+    })
+    .finally(resultOrError => {
+      this.progress.complete();
+    });
+  }
+ 
+}
